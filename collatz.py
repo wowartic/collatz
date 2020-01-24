ab3aaaf40433ed45fd58fc7dde053750d6fe0113
wowartic <wowartic@gmail.com>
2020-01-24 12:57:30 +0100

    added collatz.py

---
+++ collatz.py
@@ -0,0 +1,12 @@
+def collatz(number):
+    while number != 1:
+        if number % 2 == 0:
+            number = number // 2
+            print(number)
+        else:
+            number = (number * 3) + 1
+            print(number)
+
+
+number = int(input("enter a number: "))
+collatz(number)
